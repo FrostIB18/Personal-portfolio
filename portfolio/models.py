459014d3f88b27717fb1a970bcc66b5bc32ee807
Ivan Burakov <ivan.burakov.prof@gmail.com>
2023-02-12 23:57:16 +0600

    Add projects, change blog page style, delete spare lines of code

--- portfolio/models.py
+++ portfolio/models.py
@@ -4,12 +4,11 @@ from django.db import models
 class Project(models.Model):
     title = models.CharField(max_length = 100)
     short_description = models.CharField(max_length = 100)
-    long_description = models.TextField(default="Here should be a short description")
+    long_description = models.TextField(default="Here should be a long description.")
     image_1 = models.ImageField(upload_to = 'portfolio/images/')
     image_2 = models.ImageField(upload_to = 'portfolio/images/', blank=True)
     image_3 = models.ImageField(upload_to = 'portfolio/images/', blank=True)
     date = models.DateTimeField(auto_now_add=True)
-    url = models.URLField(blank = True)
 
     def __str__(self):
         return self.title
